5181b0ca537fc1077aff0787fb331a1271a758e6
alexey vorovich <pgmabv99@gmail.com>
2024-03-27 17:01:52 -0400

    bb

---
+++ hackerrank/bribes.py
@@ -0,0 +1,17 @@
+def minimum_bribes(q):
+    bribes = 0
+    for i in range(len(q)):
+        iold=q[i]-1
+        print(i, iold)
+        if i - iold  > 2:  # Check if any person has bribed more than two others
+            print("Too chaotic")
+            return
+        for j in range(max(0, q[i] - 2), i):  # Check how many times person i has been bribed
+            if q[j] > q[i]:
+                bribes += 1
+    print(bribes)
+
+# Example usage:
+# queue = [2, 1, 5, 3, 4]  # Example queue order
+queue = [2, 3, 5, 4 , 1]  # Example queue order
+minimum_bribes(queue)
